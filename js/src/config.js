3cb38735fc73cc72cd4c3616d095069e502a76ed
Lee Ellam <lee.ellam@reedonline.co.uk>
2014-10-17 16:22:43 +0100

    change knockout to external resource

--- js/src/config.js
+++ js/src/config.js
@@ -5,7 +5,7 @@ require.config({
   urlArgs: '_=' + (new Date()).getTime(),
   paths: {
     jquery: '../vendor/jquery/jquery.min',
-    knockout: '../vendor/knockout/build/output/knockout-latest.debug',
+    knockout: '//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-min',
     underscore: '../vendor/underscore/underscore-min',
     baseviewmodel: '../vendor/baseviewmodel/baseviewmodel.min',
     app: 'app'
